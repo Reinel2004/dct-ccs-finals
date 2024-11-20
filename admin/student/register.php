05a43dfd6067f85d71023aadb48ac5ca1452bada
Reinel Gueco <venvengueco@gmail.com>
2024-11-20 15:27:01 +0800

    removed some code

--- admin/student/register.php
+++ admin/student/register.php
@@ -4,17 +4,11 @@
     include('../../functions.php');
     include('../partials/header.php'); 
 
-    if (empty($_SESSION['email'])) {
+    if (!empty($_SESSION['email'])) {
         header("Location: ../../index.php");
         exit;
     }
 
-    header("Cache-Control: no-store, no-cache, must-revalidate"); 
-    header("Cache-Control: post-check=0, pre-check=0", false); 
-    header("Pragma: no-cache");
-
-    checkUserSessionIsActive();  
-    guard();
 
     $errors = [];
     
