129e307c13bcbe2e06d56d2e6dc67feac691ad8a
Reinel Gueco <venvengueco@gmail.com>
2024-11-21 23:28:10 +0800

    detach function added

--- functions.php
+++ functions.php
@@ -636,6 +636,30 @@
         return $subjects;
     }
     
+    function detachSubjectFromStudent($student_id, $subject_id) {
+        $conn = con();
+        $sql = "DELETE FROM students_subjects WHERE student_id = ? AND subject_id = ?";
+        
+        $stmt = mysqli_prepare($conn, $sql);
+        
+        if ($stmt) {
+            mysqli_stmt_bind_param($stmt, "ii", $student_id, $subject_id);
+            mysqli_stmt_execute($stmt);
+            
+            if (mysqli_stmt_affected_rows($stmt) > 0) {
+                mysqli_stmt_close($stmt);
+                mysqli_close($conn);
+                return true; 
+            } else {
+                mysqli_stmt_close($stmt);
+                mysqli_close($conn);
+                return false; 
+            }
+        } else {
+            mysqli_close($conn);
+            return false; 
+        }
+    }
     
     
     
