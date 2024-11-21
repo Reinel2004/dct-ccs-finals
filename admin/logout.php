865853c0a13501292e716f88b9bf142b8c78947e
Reinel Gueco <venvengueco@gmail.com>
2024-11-22 03:56:45 +0800

    logout code added

--- admin/logout.php
+++ admin/logout.php
@@ -1,7 +1,12 @@
 <?php
     // Logout Code Here
-    include '../functions.php';
     session_start();
-    $loginForm = '../index.php';
-    logOut($loginForm);
+    session_destroy(); 
+
+    header("Cache-Control: no-store, no-cache, must-revalidate"); 
+    header("Cache-Control: post-check=0, pre-check=0", false); 
+    header("Pragma: no-cache");
+
+    header("Location: ../index.php");
+exit;
 ?>
