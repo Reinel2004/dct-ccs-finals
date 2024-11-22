2e365c09a746b6ebd9b7d419a53a3eec39dee263
Reinel Gueco <venvengueco@gmail.com>
2024-11-22 11:25:41 +0800

    guard implemented into dashboard for more security

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -3,6 +3,15 @@
 <?php
     include '../functions.php';
     include './partials/header.php';
+    
+    
+    header("Cache-Control: no-store, no-cache, must-revalidate"); 
+    header("Cache-Control: post-check=0, pre-check=0", false); 
+    header("Pragma: no-cache");
+    
+    checkUserSessionIsActiveDashboard();  
+    guardDashboard(); 
+
     include './partials/side-bar.php';
 
     $pageTitle = "Dashboard";
